977e24c86c89224368eab0f0411e44b8d0b6d611
Luis Carlos Oscátegui <luis@xurface.com>
2018-12-03 21:44:25 -0500

    Minifying Bundle Files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   resolve: {
@@ -7,14 +8,37 @@ module.exports = {
       path.resolve('./node_modules')
     ]
   },
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  entry: {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby'
+    ],
+    app: ['./lib/renderers/dom.js']
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
     ]
-  }
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          test: path.resolve(__dirname, 'node_modules'),
+          name: 'vendor',
+          enforce: true
+        }
+      }
+    }
+  },
 };
